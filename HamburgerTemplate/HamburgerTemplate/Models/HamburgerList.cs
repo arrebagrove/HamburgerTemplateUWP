5c1a27ecd3b9490cff44800a058940ba50b8387a
Stefan Janssen <info@stefanjanssen.eu>
2016-01-31 15:04:24 +0100

    Hamburger Template done
    Made this Hamburger Template based on a hamburger menu I found online.
    I can't find the link anymore so if you know what this was based on tell
    me. Credits to the person who made it :p

---
+++ HamburgerTemplate/HamburgerTemplate/Models/HamburgerList.cs
@@ -0,0 +1,22 @@
+﻿using HamburgerTemplate.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamburgerTemplate.Models
+{
+	class HamburgerList
+	{
+		public string BackgroundColor { get; set; }
+		public List<HamburgerItem> MenuItems { get; set; }
+		public HamburgerList()
+		{
+			MenuItems = new List<HamburgerItem>() {
+				new HamburgerItem("\uE1CE","Item1", typeof(Start)),
+				new HamburgerItem("\uE1CF","Item2", typeof(PageNotFound))
+			};
+		}
+	}
+}
